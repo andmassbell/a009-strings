56cd0feb87409b61c805684cc2a07bb12b8e76b1
andmassbell <andremassenabelo@gmail.com>
2022-08-31 15:12:41 -0300

    exercicios-strings

---
+++ meus-exercicios/exercicio-1/index.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual é o seu nome?")
+const comFav = prompt("Quais são suas comidas favoritas? Cite o primeiro prato.")
+const comFav2 = prompt("Segundo prato:")
+const comFav3 = prompt("Terceiro prato:")
+
+console.log(`Estas são as comidas favoritas de ${nome}:\n \n \nPrato 1, ${comFav}.\n\nPrato 2, ${comFav2}.\n \nPrato 3, ${comFav3}.`)
